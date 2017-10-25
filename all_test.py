74c3b9233cde55e7766ce455de4650e569a310b1
Vinayak <vinayakkaushikk@gmail.com>
2017-10-25 23:42:04 +0530

    insertion sort and selection sort done

--- all_test.py
+++ all_test.py
@@ -1,4 +1,6 @@
 import searching
+import sorting
+import random
 
 class TestSearching(object):
     def test_binary1(self):
@@ -9,3 +11,24 @@ class TestSearching(object):
         assert searching.recursive_binary_search(searching.arr,0,len(searching.arr)-1,5) == 5
     def test_recursive2(self):
         assert searching.recursive_binary_search(searching.arr,0,len(searching.arr)-1,11) == "Not-Found"
+
+class TestSorting(object):
+    def test_selection_sort(self):
+        array = sorting.selection_sort(sorting.arr)
+        for i in range(5):
+            a = random.randint(0,len(array)-1)
+            b = random.randint(0,len(array)-1)
+            if (a > b):
+                assert array[a] > array[b]
+            elif (a <= b):
+                assert array[a] <= array[b]
+    def test_insertion_sort(self):
+        array = sorting.insertion_sort(sorting.arr)
+        for i in range(5):
+            a = random.randint(0,len(array)-1)
+            b = random.randint(0,len(array)-1)
+            if (a > b):
+                assert array[a] > array[b]
+            elif (a <= b):
+                assert array[a] <= array[b]
+ 
---
+++ sorting.py
@@ -0,0 +1,43 @@
+# implementation of different sorting algorithms
+
+import random
+
+# Initialize a random array
+
+arr = list()
+
+for i in range(20):
+    arr.append(random.randint(0,30))
+
+def selection_sort(array):
+    n = len(array)
+    for i in range(n):
+        smallest = i
+        for j in range(i,n):
+            if array[smallest] > array[j]:
+                smallest = j
+        temp = array[i]
+        array[i] = array[smallest]
+        array[smallest] = temp
+    return array
+
+def insertion_sort(array):
+    n = len(array)
+    for i in range(1,n):
+        key = array[i]
+        j = i-1
+        while (j >= 0 and array[j] > key):
+            array[j+1] = array[j]
+            j = j-1
+        array[j+1] = key
+    return array
+
+def merge_sort(array,p,r):
+    if (p >= r):
+        return array
+    q = floor((p+r)/2)
+    merge_sort(array,p,q)
+    merge_sort(array,q+1,r)
+    merge(array,p,q,r)
+
+def merge(
